d26a3b6b1f8c0951576b504e31119724f8074bb7
brydave <bryandhunt@gmail.com>
2016-04-07 23:56:41 -0400

    finishing the punches

--- time.rb
+++ time.rb
@@ -12,17 +12,33 @@ def howOld
   englishMonth = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
 
   require 'date'
-  now = Date.today
-  oldestPerson = now - (365 * 120)
-  
-  until (year > oldestPerson) && (year < now)
-    puts "What year were you born?"
-    if year < oldestPerson
-      puts "C'mon, you're not that old!"
-    elsif year > now
-      puts "C'mon, you weren't born yesterday!"
+  now = Date.today.year
+  oldestPerson = now - 120
+
+  puts "What year were you born?"
+  year = 0
+
+  while year == 0
+    year = gets.chomp
+
+    if (year.to_i > oldestPerson) && (year.to_i < now)
+      break
+    
+    elsif year.to_i < oldestPerson
+      puts "C'mon, you're not that old! Try again..."
+      year = 0
+
+    elsif year.to_i > now
+      puts "I don't think time travel has been invented... yet... Or has it? Try again."
+      year = 0
+
+    elsif year.is_a? String
+      year = 0
+      puts "I don't think that's a year... Try again!"
+
     else
-      year = gets
+      year = 0
+      puts "I don't think that's a year... Try again!"
     end
   end
 
@@ -30,15 +46,13 @@ def howOld
   month = 0
 
   while month == 0
-    month = gets.chomp
+    month = gets.downcase.chomp
 
     if englishMonth.include?(month)
       month = englishMonth.index(month) + 1
-      puts "english month"
       break
 
     elsif (month.to_i < 13) && (month.to_i > 0)
-      puts "numbered month"
       break
 
     else
@@ -47,23 +61,35 @@ def howOld
     end
 
   end
-        
 
-  
-    
+  puts "What day were you born?"
+  day = 0
 
+  while day == 0
+    day = gets.chomp
 
+    if month.to_i == 2 && day.to_i > 29
+      puts "February doesn't have that many days... Try again."      
+      day = 0
 
-  # puts "What day?"
-  # day = gets
+    elsif (day.to_i < 31) && (day.to_i > 0)
+      break
 
-  # birth = Time.mktime(year, month, day)
-  # date  = Time.new
-  # age   = date - birth
+    else
+      puts "I don't think that's right... Try again!"
+      day = 0
+    end
+  end
 
-  # bdays = age / (525600 * 60)
-  # sbday = bdays.floor
-  # puts "Well, you get #{sbday.to_s} punches!"
+  birth = Time.mktime(year, month, day)
+  puts birth
+
+  date  = Time.new
+  age   = date - birth
+
+  bdays = age / (525600 * 60)
+  sbday = bdays.floor
+  puts "Well, you get #{sbday.to_s} punches!"
   
 end
 
